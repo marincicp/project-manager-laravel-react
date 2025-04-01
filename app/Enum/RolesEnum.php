ae3dd43ad59a92effc044eeadc3b90f16bd47c37
Petar <marincicpetar@gmail.com>
2025-04-01 22:23:54 +0200

    add Roles enum

---
+++ app/Enum/RolesEnum.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enum;
+
+enum RolesEnum: string
+{
+    case Admin = "admin";
+    case User = "user";
+    case Commenter = "commenter";
+
+    public static function labels(): array
+    {
+        return [
+            self::Admin->value => "Admin",
+            self::User->value => "User",
+            self::Commenter => "Commenter"
+        ];
+    }
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Admin->value => "Admin",
+            self::User->value => "User",
+            self::Commenter => "Commenter"
+        };
+    }
+}
