bb15af1570d6d7010ca2f0fa5acfdf09193758da
Petar <marincicpetar@gmail.com>
2025-04-02 12:02:12 +0200

    add migrations

---
+++ database/migrations/2025_04_02_095403_create_upvotes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Feature;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('upvotes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Feature::class)->constrained()->cascadeOnDelete();
+            $table->boolean("upvote");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('upvotes');
+    }
+};
