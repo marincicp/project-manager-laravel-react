2a5bb570b01092ea24f347eaf911ff785013aa91
Petar <marincicpetar@gmail.com>
2025-04-03 13:34:46 +0200

    add Feature types

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -1,14 +1,30 @@
-import { Config } from 'ziggy-js';
+import { Config } from "ziggy-js";
 
 export interface User {
     id: number;
     name: string;
     email: string;
     email_verified_at?: string;
+    permissions?: string[];
+    roles?: string[];
 }
 
+export type PaginatedData<T = any> = {
+    data: T[];
+    links: Record<string, string>;
+    // meta: object;
+};
+
+export type Feature = {
+    id: number;
+    created_at: string;
+    description: string;
+    name: string;
+    user: User;
+};
+
 export type PageProps<
-    T extends Record<string, unknown> = Record<string, unknown>,
+    T extends Record<string, unknown> = Record<string, unknown>
 > = T & {
     auth: {
         user: User;
