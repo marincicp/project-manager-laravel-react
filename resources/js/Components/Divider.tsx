9050f83fde41c1dfe24f07b5298a042c8aa840ce
Petar <marincicpetar@gmail.com>
2025-04-06 22:43:30 +0200

    add Divider component

---
+++ resources/js/Components/Divider.tsx
@@ -0,0 +1,7 @@
+export default function Divider() {
+    return (
+        <div>
+            <div className="bg-white/10 my-10 h-px w-full"></div>
+        </div>
+    );
+}
--- resources/js/Components/FeatureItem.tsx
+++ resources/js/Components/FeatureItem.tsx
@@ -27,7 +27,7 @@ export default function FeatureItem({
                     {feature.description &&
                         (feature.description.length > 200 ? (
                             <>
-                                <p className=" text-gray-400">
+                                <p className="text-gray-400">
                                     {isExpanded
                                         ? feature?.description
                                         : feature?.description.slice(0, 500) +
@@ -48,6 +48,15 @@ export default function FeatureItem({
                                 {feature.description}
                             </p>
                         ))}
+
+                    <div className="p-2">
+                        <Link
+                            className="inline-flex items-center rounded-md border border-transparent bg-gray-800 px-4 py-2 text-xs font-semibold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gray-700 focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-gray-900 dark:bg-gray-600 dark:text-gray-800 dark:hover:bg-white dark:focus:bg-white dark:focus:ring-offset-gray-800 dark:active:bg-gray-300"
+                            href={route("feature.show", feature)}
+                        >
+                            Comments
+                        </Link>
+                    </div>
                 </div>
                 <div>
                     <FeatureActionDropdown feature={feature} />
