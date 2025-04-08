97f2db691edb0d27668e26139f9fd0bddf65e0e8
Petar <marincicpetar@gmail.com>
2025-04-08 13:29:28 +0200

    refactored Comment model to use morph relationship

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,17 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Comment extends Model
 {
 
+
+
+    protected $fillable = ["user_id", "comment"];
+
+    /**
+     * Get the parent commentable model (project or feature)
+     * @return \Illuminate\Database\Eloquent\Relations\MorphTo<Model, Comment>
+     */
+    public function commentable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function feature(): BelongsTo
-    {
-        return $this->belongsTo(Feature::class);
     }
 }
