18d7ef2773b74cef55581722787d1932c4bb8250
Petar <marincicpetar@gmail.com>
2025-04-14 22:03:56 +0200

    user route

---
+++ routes/user.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Enum\RolesEnum;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(["auth", "verified", "role:" . RolesEnum::Admin->value])->group(function () {
+
+   Route::get("/user", [UserController::class, "index"])->name("user.index");
+   Route::get("/user/{user}/edit", [UserController::class, "edit"])->name("user.edit");
+   Route::put("/user/{user}", [UserController::class, "update"])->name("user.update");
+});
