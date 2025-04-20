69881b33177c7ec9c46338ba23f72da00c1d78cb
Petar <marincicpetar@gmail.com>
2025-04-20 20:24:08 +0200

    add migration - projects table

---
+++ database/migrations/2025_04_19_190422_create_projects_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->foreignIdFor(User::class, "created_by");
+            $table->longText("description")->nullable();
+            $table->foreignId("status_id")->default(1)->constrained("project_statuses");
+            $table->datetime("start_date")->nullable();
+            $table->datetime("completed_at")->nullable();
+            $table->datetime("due_date")->nullable();
+        });
+    }
+};
--- database/migrations/2025_04_20_150745_add_project_id_to_features_table.php
+++ database/migrations/2025_04_20_150745_add_project_id_to_features_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,10 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('project_status', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string("name");
+        Schema::table('features', function (Blueprint $table) {
+            $table->foreignIdFor(Project::class);
         });
     }
 };
