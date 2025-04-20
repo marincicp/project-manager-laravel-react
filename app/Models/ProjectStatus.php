80e403dbfbdb74eb8bbc784f8afcdfd79d7292b3
Petar <marincicpetar@gmail.com>
2025-04-20 20:25:02 +0200

    add project and project status models

---
+++ app/Models/ProjectStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class ProjectStatus extends Model
+{
+
+
+
+    public function project(): HasMany
+    {
+
+        return $this->hasMany(Project::class);
+    }
+}
