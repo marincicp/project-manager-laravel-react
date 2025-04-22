36c7ce08fd0c5e1b7b35d00dae97fe356474ccb4
Petar <marincicpetar@gmail.com>
2025-04-22 20:14:37 +0200

    add Project Resource

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    public static $wrap = false;
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+
+
+    public function toArray(Request $request): array
+    {
+
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "description" => $this->description,
+            "completed_at"  => $this->completed_at,
+            "start_date"  => $this->start_date,
+            "due_date"  => $this->due_date,
+            "user" => new UserResource($this->user),
+            "status" => new ProjectStatusResource($this->status)
+        ];
+    }
+}
