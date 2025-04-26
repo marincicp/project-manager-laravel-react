9ea19bd25a4f74513c2ab4ac5ac2376c51e8d5ab
Petar <marincicpetar@gmail.com>
2025-04-26 08:53:04 +0200

    Login page - add create account link

--- resources/js/Pages/Auth/Login.tsx
+++ resources/js/Pages/Auth/Login.tsx
@@ -90,6 +90,13 @@ export default function Login({
                 </div>
 
                 <div className="mt-4 flex items-center justify-end">
+                    <Link
+                        href={route("register")}
+                        className="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800 mr-4"
+                    >
+                        Create an account
+                    </Link>
+
                     {canResetPassword && (
                         <Link
                             href={route("password.request")}
