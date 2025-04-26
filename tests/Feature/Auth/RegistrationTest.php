904a5f86c474388828f39121a4fc7d41daacb851
Petar <marincicpetar@gmail.com>
2025-04-26 08:52:29 +0200

    Registration test fix

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Enum\PermissionEnum;
+use App\Enum\RolesEnum;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
 test('registration screen can be rendered', function () {
     $response = $this->get('/register');
 
@@ -7,13 +12,32 @@ test('registration screen can be rendered', function () {
 });
 
 test('new users can register', function () {
+    $userRole = Role::create(['name' => RolesEnum::User->value]);
+    $adminRole = Role::create(['name' => RolesEnum::Admin->value]);
+    $commenterRole = Role::create(['name' => RolesEnum::Commenter->value]);
+
+    $manageFeaturesPermission = Permission::create(['name' => PermissionEnum::ManageFeatures->value]);
+    $manageUsersPermission = Permission::create(['name' => PermissionEnum::ManageUsers->value]);
+    $manageCommentersPermission = Permission::create(['name' => PermissionEnum::ManageComments->value]);
+    $upvoteDownvotesPermission = Permission::create(['name' => PermissionEnum::UpvoteDownvote->value]);
+
+    $userRole->syncPermissions([$upvoteDownvotesPermission]);
+    $commenterRole->syncPermissions([$upvoteDownvotesPermission, $manageCommentersPermission]);
+    $adminRole->syncPermissions([
+        $manageCommentersPermission,
+        $manageUsersPermission,
+        $manageFeaturesPermission,
+        $upvoteDownvotesPermission,
+    ]);
+
+
     $response = $this->post('/register', [
         'name' => 'Test User',
-        'email' => 'test@example.com',
+        'email' => 'tesqt@example.com',
         'password' => 'password',
         'password_confirmation' => 'password',
     ]);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('dashboard', absolute: false));
+    $response->assertRedirect(route('dashboard', false));
 });
