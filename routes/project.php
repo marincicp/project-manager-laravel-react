e305ed821322fa1ba518bbe4c697d65465600311
Petar <marincicpetar@gmail.com>
2025-04-27 22:17:24 +0200

    Project routes - permission fix

--- routes/project.php
+++ routes/project.php
@@ -7,7 +7,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(["auth", "verified"])->group(function () {
 
-   Route::resource("/project", ProjectController::class);
+   Route::resource('project', ProjectController::class)->except(['index', 'show'])->middleware('can:' . PermissionEnum::ManageFeatures->value);
+
+
+   Route::get('/project', [ProjectController::class, 'index'])->name('project.index');
+   Route::get('/project/{project}', [ProjectController::class, 'show'])->name('project.show');
 
 
    Route::post("/project/{project}/comment", [ProjectCommentController::class, "store"])->middleware('can:' . PermissionEnum::ManageComments->value)->name("projectComment.store");
