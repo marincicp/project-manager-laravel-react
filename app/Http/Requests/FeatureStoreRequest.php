10107962138460ff0ce5670f42101146e125c22d
Petar <marincicpetar@gmail.com>
2025-04-28 22:02:29 +0200

    add FeatureStoreRequest

---
+++ app/Http/Requests/FeatureStoreRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FeatureStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['string', 'min:5', 'max:500', 'required'],
+            'description' => ['nullable', 'string'],
+            "project_id" => ["required", "exists:projects,id"]
+        ];
+    }
+}
