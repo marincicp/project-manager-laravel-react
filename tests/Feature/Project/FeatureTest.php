abef658db54bc48698e790abc5cf269cec03b39b
Petar <marincicpetar@gmail.com>
2025-05-05 21:33:20 +0200

    Feature test -admin can create feature

---
+++ tests/Feature/Project/FeatureTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Enum\RolesEnum;
+use App\Models\Project;
+use App\Models\User;
+use Database\Seeders\ProjectStatusesSeeder;
+use Database\Seeders\RolesAndPermissionSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('can be created by admin role', function () {
+
+   $this->seed([RolesAndPermissionSeeder::class, ProjectStatusesSeeder::class]);
+
+   $user = User::factory()->create([
+      'email_verified_at' => now(),
+   ]);
+   $user->assignRole(RolesEnum::Admin->value);
+
+   $project = $user->projects()->create(['name' => 'New Project', 'description' => "desc"]);
+
+   $this->actingAs($user)->get(route('feature.create'))->assertStatus(200);
+
+   $res = $this->actingAs($user)->post(
+      '/feature',
+      [
+         'name' => 'Feature x',
+         'description' => 'some description',
+         'project_id' => $project->id,
+      ]
+   );
+
+   $res->assertRedirect(route('feature.index'));
+   $this->assertDatabaseHas('features', [
+      'name' => 'Feature x',
+   ]);
+});
